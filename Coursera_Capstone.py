3e6576997cce6572d102430600a93698648944c8
KKaatthhyy <53499085+KKaatthhyy@users.noreply.github.com>
2019-07-30 17:58:40 -0700

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# # Capstone
+
+# ## This is about Coursera capstone.
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print("Hello Capstone Project Course!")
+
